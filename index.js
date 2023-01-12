823bd890bbd9a83333b41db25ef2d4e531caff08
Pema Ngodup <pngodup123@gmail.com>
2023-01-12 12:52:30 +0000

    installed mongoose orm package

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
+dotenv.config({ path: './config/config.env' });
 //JSON Parser
 app.use(express.json());
 
@@ -14,12 +16,16 @@ if (process.env.NODE_ENV === 'development') {
 //Route file
 const homeRoute = require('./routes/homeRoute');
 
-dotenv.config({ path: './config/config.env' });
-
 //
 app.use('/v1/cv/api', homeRoute);
 
 //
-app.listen(process.env.PORT, () => {
-  console.log('App running in the port no 5000');
-});
+connectDB()
+  .then(
+    app.listen(process.env.PORT, () => {
+      console.log('App running in the port no 5000');
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
