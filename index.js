40c72ae11a69fbb02a5430d2388899fb1ce19074
Pema Ngodup <pngodup123@gmail.com>
2023-01-17 13:46:53 +0000

    cors middleware changes

--- index.js
+++ index.js
@@ -15,18 +15,10 @@ app.use(
     ],
   })
 );
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-//   );
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 dotenv.config({ path: './config/config.env' });
 //JSON Parser
