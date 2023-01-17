bf0c0709f6cd20ffe59f37705e97e33a2e39060f
Pema Ngodup <pngodup123@gmail.com>
2023-01-17 10:16:24 +0000

    Changes in db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,12 @@ const connectDB = async () => {
   if (process.env.NODE_ENV === 'development') {
     mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.9wgye.mongodb.net/${process.env.MONGO_DATABASE_NAME}`
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.9wgye.mongodb.net/${process.env.MONGO_DATABASE_NAME}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        family: 4,
+      }
     );
   }
 };
